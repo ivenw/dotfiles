fadbd28edb910534840b91d4e6b605974c89596d
Iven Winkelmann <winkelmann.iven@gmail.com>
2022-10-09 13:06:34 +0200

    Update dot_zshrc Update run_once_2_pipx-installs.sh

--- run_once_2_pipx-installs.sh
+++ run_once_2_pipx-installs.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Poetry
 pipx install poetry
+mkdir $ZSH_CUSTOM/plugins/poetry
+poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry
+
 pipx install pre-commit
 pipx install python-lsp-server
