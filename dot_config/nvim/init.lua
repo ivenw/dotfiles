97d72f6bfd64ae4c311a869c2bd55475daa81392
Iven Winkelmann <winkelmann.iven@gmail.com>
2023-03-13 22:29:51 +0100

    add nvim

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,32 @@
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- load plugins
+require("lazy").setup({
+  "github/copilot.vim", 
+  {
+  "folke/which-key.nvim",
+  config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  }
+})
+
