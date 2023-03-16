a5b479b35bacd68be16cdf5fc2f3da1bf3f4566b
Iven Winkelmann <winkelmann.iven@gmail.com>
2023-03-16 10:00:08 +0100

    nvim config

--- dot_config/nvim/lua/vimconfig.lua
+++ dot_config/nvim/lua/vimconfig.lua
@@ -5,3 +5,24 @@ vim.g.maplocalleader = ' '
 
 -- Enable true colors
 opt.termguicolors = true
+
+-- line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- tab settings
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursorline
+opt.cursorline = true
