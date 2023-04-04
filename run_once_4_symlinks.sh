464d62ed4683885e980880048b58bff8b2b17c17
Iven Winkelmann <winkelmann.iven@gmail.com>
2023-04-04 09:56:54 +0200

    fix typo in command

--- run_once_4_symlinks.sh
+++ run_once_4_symlinks.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 echo "#! /bin/sh\ndotnet ~/Users/ivenwinkelmann/.local/bicep-langserver/Bicep.LangServer.dll" > ~/.local/bin/bicep-langserver
-chdmod 755 ~/.local/bicep-langserver
+chmod 755 ~/.local/bicep-langserver
 
