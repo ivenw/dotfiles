e557f7242082f99117bea923732aa3e2f62ea178
Iven Winkelmann <winkelmann.iven@gmail.com>
2023-06-16 09:47:28 +0200

    deactivate bicep and add java home path

--- run_once_4_symlinks.sh
+++ run_once_4_symlinks.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-echo "#! /bin/sh\ndotnet ~/Users/ivenwinkelmann/.local/bicep-langserver/Bicep.LangServer.dll" > ~/.local/bin/bicep-langserver
-chmod 755 ~/.local/bicep-langserver
+#echo "#! /bin/sh\ndotnet ~/Users/ivenwinkelmann/.local/bicep-langserver/Bicep.LangServer.dll" > ~/.local/bin/bicep-langserver
+#chmod 755 ~/.local/bicep-langserver
 
